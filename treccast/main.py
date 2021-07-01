098c0f779c0f63f9bb36e400c98c0c58b9377bc4
Krisztian Balog <krisztian.balog@uis.no>
2021-07-01 09:59:24 +0200

    Add 2020 topics and main skeleton

--- treccast/main.py
+++ treccast/main.py
@@ -1,7 +1,23 @@
 """Main command line application."""
 
+from typing import List
+
+
+def retrieval(topics: List[Topic]) -> None:
+
+    for topic in topics:
+        for turn_id in range(1, topic.num_turns() + 1):
+            pass
+            # question, context = topic.get_turn(turn_id)
+            # query = get_query(question, context)
+            # initial_ranking = do_first_pass_retrieval(query)
+            # query2 = rewrite_query(query, initial_ranking)
+            # final_ranking = rerank(initial_ranking, query2)
+
+
 if __name__ == "__main__":
     # TODO: add argparse
     # Different modes:
+    topics = ...  # List[Topic]
     # - query rewriting (input: TREC CAsT topic file, output: query rewrites file)
     # - retrieval using queries from file
