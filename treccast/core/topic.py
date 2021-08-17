c4007dc9c8a3e2a3bce2e794f243212d1e88ec61
Ivica Kostric <ivica_kostric@yahoo.com>
2021-08-17 13:27:28 +0200

    Convert Turn and Topic classes to dataclass. (#94)

--- treccast/core/topic.py
+++ treccast/core/topic.py
@@ -5,89 +5,29 @@ of a Question and Context.
 import json
 
 from typing import List, Tuple
+from dataclasses import dataclass
 
 
+@dataclass
 class Turn:
-    def __init__(
-        self,
-        result_turn_dependence: int,
-        canonical_result_id: str,
-        turn_id: int,
-        manual_rewritten_utterance: str,
-        automatic_rewritten_utterance: str,
-        query_turn_dependence: list,
-        raw_utterance: str,
-    ) -> None:
-        """Create a Turn object with Args as Attributes.
+    turn_id: int
+    canonical_result_id: str
+    result_turn_dependence: int
+    query_turn_dependence: list
+    raw_utterance: str
+    automatic_rewritten_utterance: str = None
+    manual_rewritten_utterance: str = None
 
-        Args:
-            result_turn_dependence: [description]
-            canonical_result_id: [description]
-            turn_id: [description]
-            manual_rewritten_utterance: [description]
-            automatic_rewritten_utterance: [description]
-            query_turn_dependence: [description]
-            raw_utterance: [description]
-        """
-        self._result_turn_dependence = result_turn_dependence
-        self._canonical_result_id = canonical_result_id
-        self._turn_id = turn_id
-        self._manual_rewritten_utterance = manual_rewritten_utterance
-        self._automatic_rewritten_utterance = automatic_rewritten_utterance
-        self._query_turn_dependence = query_turn_dependence
-        self._raw_utterance = raw_utterance
-        # TODO: Extend with the text of the canonical result
-        # See https://github.com/iai-group/trec-cast-2021/issues/17
-
-    @property
-    def turn_id(self) -> int:
-        return self._turn_id
-
-    @property
-    def raw_utterance(self) -> str:
-        return self._raw_utterance
-
-    @property
-    def manual_utterance(self) -> str:
-        return self._manual_rewritten_utterance
-
-    @property
-    def automatic_utterance(self) -> str:
-        return self._automatic_rewritten_utterance
+    # TODO: Extend with the text of the canonical result
+    # See https://github.com/iai-group/trec-cast-2021/issues/17
 
 
+@dataclass
 class Topic:
-    def __init__(
-        self, topic_id: int, description: str, title: str, turns: List[Turn]
-    ) -> None:
-        """Initializes a topic.
-
-        Args:
-            topic_id: Topic ID (as provided by TREC).
-            description: Topic description.
-            title: Topic title.
-            turns: List of conversation turns.
-        """
-        self._topic_id = topic_id
-        self._description = description
-        self._title = title
-        self._turns = turns
-
-    @property
-    def topic_id(self) -> int:
-        return self._topic_id
-
-    @property
-    def description(self) -> str:
-        return self._description
-
-    @property
-    def title(self) -> str:
-        return self._title
-
-    @property
-    def turns(self) -> List[Turn]:
-        return self._turns
+    topic_id: int
+    description: str
+    title: str
+    turns: List[Turn]
 
     def get_turn(self, turn_id: int) -> Turn:
         """Returns a given topic turn.
@@ -101,9 +41,9 @@ class Topic:
         Returns:
             Turn instance.
         """
-        if len(self._turns) < turn_id:
+        if len(self.turns) < turn_id:
             raise IndexError(f"Invalid turn_id: {turn_id}")
-        return self._turns[turn_id - 1]
+        return self.turns[turn_id - 1]
 
     def get_question_and_context(
         self, turn_id: int, utterance_type: str = "raw"
@@ -122,14 +62,15 @@ class Topic:
         if utterance_type == "raw":
             utterance = turn.raw_utterance
         elif utterance_type == "automatic":
-            utterance = turn.automatic_utterance
+            utterance = turn.automatic_rewritten_utterance
         elif utterance_type == "manual":
-            utterance = turn.manual_utterance
+            utterance = turn.manual_rewritten_utterance
         else:
             raise ValueError(
-                "Incorrect utterance type. Accepted values are: 'raw', 'manual', and 'automatic'."
+                "Incorrect utterance type. Accepted values are: 'raw', 'manual'"
+                ", and 'automatic'."
             )
-        return utterance, self._turns[: turn_id - 1]
+        return utterance, self.turns[: turn_id - 1]
 
 
 def construct_topics_from_file(filepath: str) -> List[Topic]:
@@ -141,37 +82,29 @@ def construct_topics_from_file(filepath: str) -> List[Topic]:
     Returns:
         Extracted Topics.
     """
-    topics = []
     with open(filepath, "r", encoding="utf8") as f_in:
         raw_topics = json.load(f_in)
-        for raw_topic in raw_topics:
-            topic_id = raw_topic.get("number")  # int
-            description = raw_topic.get("description")  # str
-            title = raw_topic.get("title")  # str
-            raw_turns = raw_topic.get("turn")  # List(dict)
-            turns = []
-            for raw_turn in raw_turns:
-                turn_id = raw_turn.get("number")
-                result_turn_dependence = raw_turn.get("result_turn_dependence")
-                canonical_result_id = raw_turn.get("canonical_result_id")
-                manual_rewritten_utterance = raw_turn.get(
-                    "manual_rewritten_utterance"
-                )
-                automatic_rewritten_utterance = raw_turn.get(
+
+    topics = []
+    for raw_topic in raw_topics:
+        topic_id = raw_topic.get("number")  # int
+        description = raw_topic.get("description")  # str
+        title = raw_topic.get("title")  # str
+        turns = [
+            Turn(
+                turn_id=raw_turn.get("number"),
+                canonical_result_id=raw_turn.get("canonical_result_id"),
+                result_turn_dependence=raw_turn.get("result_turn_dependence"),
+                query_turn_dependence=raw_turn.get("query_turn_dependence"),
+                raw_utterance=raw_turn.get("raw_utterance"),
+                automatic_rewritten_utterance=raw_turn.get(
                     "automatic_rewritten_utterance"
-                )
-                query_turn_dependence = raw_turn.get("query_turn_dependence")
-                raw_utterance = raw_turn.get("raw_utterance")
-                turn = Turn(
-                    result_turn_dependence,
-                    canonical_result_id,
-                    turn_id,
-                    manual_rewritten_utterance,
-                    automatic_rewritten_utterance,
-                    query_turn_dependence,
-                    raw_utterance,
-                )
-                turns.append(turn)
-            topic = Topic(topic_id, description, title, turns)
-            topics.append(topic)
+                ),
+                manual_rewritten_utterance=raw_turn.get(
+                    "manual_rewritten_utterance"
+                ),
+            )
+            for raw_turn in raw_topic.get("turn")
+        ]
+        topics.append(Topic(topic_id, description, title, turns))
     return topics
