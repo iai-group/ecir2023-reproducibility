84f23a2c1a2233030eef6a149ca811a9471d1e77
Ivica Kostric <ivica_kostric@yahoo.com>
2022-06-24 13:46:19 +0200

    Archive repo and remove analyzer scripts (#282)

--- scripts/finetuned_rerankers_2020_all.sh
+++
@@ -1,7 +0,0 @@
-METHOD_LIST=( raw manual automatic )
-PREV_LIST=( 0 1 2 3 4 5 all )
-for method in "${METHOD_LIST[@]}"; do	
-    for n in "${PREV_LIST[@]}"; do	
-        CUDA_VISIBLE_DEVICES=1 python -m treccast.main -y 2020 --config-file config/fine_tuning/2020_${method}_prev_${n}_msmarco_treccast_wow.yaml
-    done
-done
--- scripts/finetuned_rerankers_2021_all.sh
+++
@@ -1,7 +0,0 @@
-METHOD_LIST=( raw manual automatic )
-PREV_LIST=( 0 1 2 3 4 5 all )
-for method in "${METHOD_LIST[@]}"; do	
-    for n in "${PREV_LIST[@]}"; do	
-        CUDA_VISIBLE_DEVICES=0 python -m treccast.main -y 2021 --config-file config/fine_tuning/2021_${method}_prev_${n}_msmarco_treccast_wow.yaml
-    done
-done
